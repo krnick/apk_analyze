6c5eacda6a6a5c49f51f6bfa2d3560bac4626f00
nick <you@example.com>
2018-12-12 15:52:18 +0800

    apk tool

--- install_requirement.sh
+++ install_requirement.sh
@@ -1,2 +1,9 @@
 #git clone https://github.com/radare/radare2.git
 radare2/sys/install.sh
+
+
+#install apktool
+sudo cp apktool/* /usr/local/bin/ 
+sudo chmod +x /usr/local/bin/apktool.jar
+sudo chmod +x /usr/local/bin/apktool
+
